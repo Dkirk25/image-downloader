25b0f80728a0acde809213a9ee56f18572ba97a8
Donald Kirk <dkirk9325@gmail.com>
2020-04-09 17:02:27 -0500

    added google platform option.

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-const qs = require('querystring');
-const path = require('path');
-const request = require('request');
+const qs = require("querystring");
+const path = require("path");
+const request = require("request");
 
-const parseHTML = require('./src/services/html-image-link-parser');
-const ImageDownloader = require('./src/services/image-downloader');
+const parseHTML = require("./src/services/html-image-link-parser");
+const ImageDownloader = require("./src/services/image-downloader");
 
-const outputDir = path.join(__dirname, 'images')
-const argv = require('yargs').argv
+const outputDir = path.join(__dirname, "images");
+const argv = require("yargs").argv;
 const downloader = new ImageDownloader(outputDir);
 
-const BING = 'https://www.bing.com/images/search'
+const BING = "https://www.bing.com/images/search";
+const GOOGLE = "https://www.google.com/search";
 
 function main(args) {
-  const { query, limit = 10} = args;
+  const { platform, query, limit = 10 } = args;
 
-  const uri = `${BING}?${qs.stringify({q: query})}`;
-  console.log('Requesting', uri);
-  request(uri, function(error, response, body) {
+  if (platform === "g") {
+    platform == GOOGLE;
+  } else {
+    platform == BING;
+  }
+
+  const uri = `${BING}?${qs.stringify({ q: query })}`;
+
+  console.log("Requesting", uri);
+  request(uri, function (error, response, body) {
     try {
       const links = parseHTML(body);
-      console.log('Links Found', links);
+      console.log("Links Found", links);
 
       if (links.length > 0) {
         links.slice(0, limit).forEach((link, i) => {
-          downloader.download({uri: link, filename: `${i}` }, function() {
-            console.log('done');
-          })
-        })
+          downloader.download({ uri: link, filename: `${i}` }, function () {
+            console.log("done");
+          });
+        });
       } else {
-        console.log(`No valid image links found for ${query}`)
+        console.log(`No valid image links found for ${query}`);
       }
     } catch (error) {
       console.error(error);
       process.exit(-1);
     }
-  })
+  });
 }
-
-
 
 main(argv);
--- src/services/html-image-link-parser.js
+++ src/services/html-image-link-parser.js
@@ -1,15 +1,18 @@
-const cheerio = require('cheerio')
+const cheerio = require("cheerio");
+
+// rg_i Q4LuWd tx8vtf
 
 function parseHtml(html) {
   const $ = cheerio.load(html);
-  const links = []
-  $('img').each(function(i, element) {
-    const link = $(element).attr('src');
-    links.push(link);
-  })
+  const links = [];
 
-  console.log('Links', links);
-  return links.filter(link => link.indexOf('http') !== -1);
+  $("img").each(function (i, element) {
+    const link = $(element).attr("src");
+    links.push(link);
+  });
+
+  console.log("Links", links);
+  return links.filter((link) => link.indexOf("http") !== -1);
 }
 
 module.exports = parseHtml;
